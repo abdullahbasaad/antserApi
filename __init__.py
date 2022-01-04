2f6a8fa63757b343b58c647b6222aa8a957849a4
Abdullah Basaad <65473122+abdullahbasaad@users.noreply.github.com>
2022-01-04 16:25:53 +0000

    me

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 api = Api(app)
 
 app.config['SECRET_KEY'] = 'Should be secret!!!'
-app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://postgres:pass@ip address/Data base name'
+app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://postgres:pass@ip-address/Database name'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = 'public-key'
